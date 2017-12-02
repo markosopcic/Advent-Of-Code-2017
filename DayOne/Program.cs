e794351847f733b6069628daf343894166cc2073
DESKTOP-HI7VO1V\Marko <marko.sopcic@fer.hr>
2017-12-02 13:14:48 +0100

    first two days

---
+++ DayOne/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOne
+{
+    class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            
+            Second(args);
+        }
+
+
+        public static void Second(String[] args)
+        {
+         
+            var sum = 0;
+            char[] chars = args[0].ToCharArray();
+            var listLen = chars.Length;
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] == chars[(i + listLen/2) % listLen])
+                {
+                    sum += chars[i]-'0';
+                }
+              
+            }
+            
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        public static void First(String[] args)
+        {
+            var len = 0;
+            var sum = 0;
+            char[] chars = args[0].ToCharArray();
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] == chars[i + 1])
+                {
+                    while (i < chars.Length - 1 && chars[i] == chars[i + 1])
+                    {
+                        len++;
+                        i++;
+                    }
+                }
+                sum += len * (chars[i] - '0');
+                len = 0;
+            }
+            if (chars[chars.Length - 1] == chars[0]) sum += chars[0] - '0';
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DayTwo/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] result = System.IO.File.ReadAllLines("C:/Users/Marko/source/repos/AdventOfCode/DayTwo/Input.txt");
+            Second(result);
+        }
+
+        private static void First(string[] args)
+        {
+            int checksum = 0;
+            for(int i = 0; i < args.Length; i++)
+            {
+                String[] line = args[i].Split('\t');
+                int min = 0;int max = 0;
+                min = int.Parse(line[0]);
+              for(int j = 0; j < line.Length; j++)
+                {
+                    int num = int.Parse(line[j]);
+                    if (num > max) max = num;
+                    if (num < min) min = num;
+                }
+                checksum += max - min;
+                max = 0;
+            }
+            Console.Write(checksum);
+            Console.ReadKey();
+        }
+
+        private static void Second(String[] args)
+        {
+            int sum = 0;
+            for(int i = 0; i < args.Length; i++)
+            {
+
+                List<int> line = new List<int>();
+                String[] lineString = args[i].Split('\t');
+                for(int j = 0; j < lineString.Length; j++)
+                {
+                    line.Add(int.Parse(lineString[j]));
+                }
+                int[] nums = line.ToArray();
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    for(int x = 0; x < nums.Length; x++)
+                    {
+                        if (x == j)
+                        {
+                            continue;
+                        }
+                        if (nums[x] % nums[j] == 0)
+                        {
+                            sum += nums[x] / nums[j];
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
